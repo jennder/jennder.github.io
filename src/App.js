0e9d9cef78f1a017298bd40aa5ecc2aa5b527045
Jenn Der <der.je@husky.neu.edu>
2020-07-05 15:11:56 -0400

    init react commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation';
+
+const App = () => {
+  return (
+    <Navigation />
+  );
+};
+
+export default App;
