52fca5cf254f90c6f0cc5208aea6a2791c08b9f7
Jenn Der <der.je@husky.neu.edu>
2020-07-06 03:13:34 -0400

    Updates

---
+++ precache-manifest.80e05bf2a90cc48a9c49a1195ebfcf12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35970428de8dba0283d06a7eecfeeb29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d36b7c3a6bcfca8d3f9f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5627fef6c6d45108ab17",
+    "url": "/static/css/main.17d2f770.chunk.css"
+  },
+  {
+    "revision": "d36b7c3a6bcfca8d3f9f",
+    "url": "/static/js/2.7562436b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7562436b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5627fef6c6d45108ab17",
+    "url": "/static/js/main.2d352260.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "56cc8b4a1879223e51989b603bcb65a8",
+    "url": "/static/media/Houston-Serif.56cc8b4a.otf"
+  },
+  {
+    "revision": "4dbbdcd595dcd961a2cab5bd9d026c80",
+    "url": "/static/media/TenorSans-Regular.4dbbdcd5.ttf"
+  }
+]);
